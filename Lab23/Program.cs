0b58bffbec5408fc5ded783c39438d7eb87641c5
Vera Kiprskaya <vera.malyutina@gmail.com>
2022-08-28 21:05:11 +0300

    Добавьте файлы проекта.

---
+++ Lab23/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n=Convert.ToInt32(Console.ReadLine());
+
+            FctrlAsync(n);
+            Console.ReadKey();
+        }
+
+        static void Fctrl(int n)
+        {
+            int f = 1;
+            if (n == 0)
+            {
+                f = 1;
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    f *= i;
+                    Thread.Sleep(100);
+                }
+            }
+            Console.WriteLine(f);
+        }
+
+        static async void FctrlAsync (int n)
+        {
+            await Task.Run(() => Fctrl(n));
+        }
+    }
+}
